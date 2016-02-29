228e2fb0d5bba5cea0f60ef713e564f69ca77c7e
Gaston Lenotre <gastonlenotre@gmail.com>
2016-02-29 13:56:53 -0600

    made a guess a number game

--- highlow.php
+++ highlow.php
@@ -1,3 +1,34 @@
 <?php
 
-High/Low game in PHP
+$min = 0;
+$max = 100;
+
+$randomNumber = rand ( $min , $max );
+
+fwrite(STDOUT, $randomNumber);
+fwrite(STDOUT, "Guess the number\n");
+$userNumber = trim(fgets(STDIN));
+
+
+do {
+	if ($userNumber < $randomNumber) {
+	    fwrite (STDOUT, "Guess higher\n");
+		// $userNumber = trim(fgets(STDIN));
+
+	} elseif ($userNumber > $randomNumber) {
+	    fwrite (STDOUT, "Guess lower\n");
+		// $userNumber = trim(fgets(STDIN));
+
+	} 	
+
+		$userNumber = trim(fgets(STDIN));
+	
+} while ($userNumber != $randomNumber);
+
+if ($userNumber == $randomNumber){
+		fwrite (STDOUT, "Dynamite!\n");
+	}
+
+
+
+
